8a5787a9a60585579abcbf4b0fc841b792880839
kyky <kyle.vuanh@gmail.com>
2025-02-05 23:13:54 +0700

    [CREATE] Navbar final

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import { GiNinjaStar } from "react-icons/gi";
 import { HiOutlineMenu } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md";
 
 function Navbar() {
+    const [openMenu, setOpenMenu] = useState(false);
+    const handleMenu = () => {
+        setOpenMenu(!openMenu);
+    }
     return (
         <nav className="flex item-center">
             <div className="flex item-center">
                 <GiNinjaStar className="size-[30px] cursor-pointer"/>
-                <div className="text-[20px] font-bold ml-2 cursor-pointer"> NR Wiki </div>
+                <div className="text-[20px] font-bold ml-1 cursor-pointer"> NR Wiki </div>
             </div>
-            <ul className="ml-auto text[16px] font-semibold">
-                <HiOutlineMenu className="size-[30px] cursor-pointer"/>
+            <input className="bg-white ml-2 rounded-xl "/>
+            <ul className="ml-auto text[20px] font-semibold">
+                {openMenu ? (
+                    <MdOutlineClose className="size-[30px] cursor-pointer" onClick={handleMenu}/>
+                ) : (
+                    <HiOutlineMenu className="size-[30px] cursor-pointer" onClick={handleMenu}/>
+                )}
+                {openMenu && (
+                    <div className="absolute right-8 bg-white p-6 text-center rounded-xl">
+                        <li className="cursor-pointer">Add ninja</li>
+                        <li className="cursor-pointer">Login</li>
+                    </div>
+                )}
             </ul>
         </nav>
     )
