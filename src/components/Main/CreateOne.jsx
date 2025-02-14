005990be6a8ad3520c54df9a77e574cf6d2da006
kyky <kyle.vuanh@gmail.com>
2025-02-15 02:57:20 +0700

    Update CreateOne.jsx

--- src/components/Main/CreateOne.jsx
+++ src/components/Main/CreateOne.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function CreateOne() {
@@ -15,7 +15,7 @@ export default function CreateOne() {
         const { name, value } = event.target;
         setCreateOne((prevData) => ({
           ...prevData,
-          [name]: value,
+          [name]: value
         }));
     }
     const handleSubmit = async (event) => {
@@ -30,7 +30,7 @@ export default function CreateOne() {
         }
 
         try {
-            const token = localStorage.getItem("Token");
+            const token = localStorage.getItem('Token');
             const response = await fetch(`http://localhost:8080`, {
                 method: 'POST',
                 headers: {
@@ -41,95 +41,90 @@ export default function CreateOne() {
             });
 
             if (response.ok) {
-                console.log(response.status);
                 navigate(`/`);
             } else if (response.status === 409) {
                 setCreateSucceed('Ninja existed');
-            } else {
-                setCreateSucceed('Create ninja failed');
+                window.scrollTo({ top: 0 });            
             }
         } catch (error) {
+            setCreateSucceed('Create ninja failed, check if user logged in or not');
+            window.scrollTo({ top: 0 });
             console.error(error);
         }
     }
     return (
-        <div className="max-w-lg mx-auto mt-10 p-5 border border-gray-300 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-semibold mb-6 text-center">Character Skills Form</h2>
+        <div className='max-w-lg mx-auto mt-10 p-5 border border-gray-300 rounded-lg shadow-lg'>
+          <h2 className='text-2xl font-semibold mb-6 text-center'>Create Ninja</h2>
           <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-                Name
-              </label>
+            {createSucceed && (
+                <div className='text-red-500 text-l mt-2 font-semibold'>
+                    {createSucceed}
+                </div>
+            )}
+            <div className='mb-4'>
+              <label className='block text-sm font-medium text-gray-700'>Name</label>
               <input
-                type="text"
-                id="name"
-                name="name"
+                type='text'
+                id='name'
+                name='name'
                 value={createOne.name}
                 onChange={handleCreate}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
+                required
               />
             </div>
-    
-            <div className="mb-4">
-              <label htmlFor="skill1" className="block text-sm font-medium text-gray-700">
-                Skill 1
-              </label>
+            <div className='mb-4'>
+              <label className='block text-sm font-medium text-gray-700'>Skill 1</label>
               <textarea
-                id="skill1"
-                name="skill1"
+                id='skill1'
+                name='skill1'
                 value={createOne.skill1}
                 onChange={handleCreate}
-                rows="4"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                rows='4'
+                className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
+                required
               />
             </div>
-    
-            <div className="mb-4">
-              <label htmlFor="skill2" className="block text-sm font-medium text-gray-700">
-                Skill 2
-              </label>
+            <div className='mb-4'>
+              <label className='block text-sm font-medium text-gray-700'>Skill 2</label>
               <textarea
-                id="skill2"
-                name="skill2"
+                id='skill2'
+                name='skill2'
                 value={createOne.skill2}
                 onChange={handleCreate}
-                rows="4"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                rows='4'
+                className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
+                required
               />
             </div>
-    
-            <div className="mb-4">
-              <label htmlFor="skill3" className="block text-sm font-medium text-gray-700">
-                Skill 3
-              </label>
+            <div className='mb-4'>
+              <label className='block text-sm font-medium text-gray-700'>Skill 3</label>
               <textarea
-                id="skill3"
-                name="skill3"
+                id='skill3'
+                name='skill3'
                 value={createOne.skill3}
                 onChange={handleCreate}
-                rows="4"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                rows='4'
+                className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
+                required
               />
             </div>
-    
-            <div className="mb-4">
-              <label htmlFor="skill4" className="block text-sm font-medium text-gray-700">
-                Skill 4
-              </label>
+            <div className='mb-4'>
+              <label className='block text-sm font-medium text-gray-700'>Skill 4</label>
               <textarea
-                id="skill4"
-                name="skill4"
+                id='skill4'
+                name='skill4'
                 value={createOne.skill4}
                 onChange={handleCreate}
-                rows="4"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                rows='4'
+                className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
+                required
               />
             </div>
-    
-            <div className="flex justify-center">
+            <div className='flex justify-center'>
               <button
-                type="submit"
-                className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                type='submit'
+                className='px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500'
               >
                 Submit
               </button>
