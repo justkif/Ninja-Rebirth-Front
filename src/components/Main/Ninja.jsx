907c56bfbcb51f70f94c26c9ba4016b1c0275893
kyky <kyle.vuanh@gmail.com>
2025-02-26 21:37:33 +0700

    Update Ninja.jsx

--- src/components/Main/Ninja.jsx
+++ src/components/Main/Ninja.jsx
@@ -1,7 +1,7 @@
 export default function Ninja({ ninja }) {
     return (
         <div className='shadow-lg rounded-xl cursor-pointer'>
-            <img src={ninja.imageurl} className='rounded-t-xl' style={{ width: '100%', height: '100%', objectFit: 'cover' }}/>
+            <img src={ninja.imageurl} className='rounded-t-xl' style={{ width: '100%', height: '1000px', objectFit: 'cover' }}/>
             <div className='font-bold text-[16px] p-2'> {ninja.name} </div>
         </div>
     );
